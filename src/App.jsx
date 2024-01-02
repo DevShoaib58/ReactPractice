4fac880871d3f01f545fc11bd03142140692a1b8
DevShoaib58 <devshoaib58@gmail.com>
2024-01-02 23:55:33 +0500

    Learing Props and Childer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 
 import "./App.css";
-import Person from "./Componets/Person";
+import RandomNumber from "./Componets/RandomNumber";
+import Props from "./Componets/Props";
 
 function App() {
   return (
     <>
       <h1>Hello React App</h1>
-      <Person />
-      <Person />
-      <Person />
+      <RandomNumber />
+      <Props name="Shoaib" age="27"></Props>
+      <Props name="Talha" age="18"></Props>
+      <Props name="Talha" age="18">
+        My hobbies are Racing
+      </Props>
+      <Props name="Bilal" age="12"></Props>
     </>
   );
 }
