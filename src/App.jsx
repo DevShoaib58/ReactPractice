03cae622927a2f4275dff02914ee34b6d712b72f
Shoaib Qureshi <shoaib@solutionsurface.com>
2024-04-24 17:46:56 +0500

    Counter is Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
 
-import "./App.css";
-import RandomNumber from "./Componets/RandomNumber";
-import Props from "./Componets/Props";
 
 function App() {
+  const addValue = () => {
+    setCounter(counter + 1)
+  }
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+  const reset = () => {
+    setCounter(counter = 0)
+  }
+  let [counter, setCounter] = useState(0)
   return (
-    <>
+    <div style={{ margin: "auto", display: "block", textAlign: "center", padding: '0px 10px', }}>
       <h1>Hello React App</h1>
-      <RandomNumber />
-      <Props name="Shoaib" age="27"></Props>
-      <Props name="Talha" age="18"></Props>
-      <Props name="Talha" age="18">
-        My hobbies are Racing
-      </Props>
-      <Props name="Bilal" age="12"></Props>
-    </>
+      <h1>Counter Value {counter}</h1>
+      <button onClick={addValue}>Add</button><br />
+      <button onClick={removeValue}>Remove</button>
+      <button onClick={reset}>Remove</button>
+    </div>
   );
 }
 
