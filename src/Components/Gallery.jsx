6b7ff2f1c0140663b1abc9bc03584840d0be6f75
Shoaib Qureshi <shoaib@solutionsurface.com>
2024-05-20 18:01:22 +0500

    Gallery Componet is completed

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -3,32 +3,43 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Gallery = () => {
-    let baseURL = "https://picsum.photos/v2/list";
-    // let baseURL = "https://api.unsplash.com/photos/?client_id=eJrGowlILBc7vmgyiBxiazUp8aXES8G-pA3yuxGQpis";
-    const [data, setData] = useState([])
+    // let baseURL = "https://picsum.photos/v2/list";
+    let GalleryKey = import.meta.env.VITE_SOME_KEY
+    console.log('GalleryKey: ', GalleryKey);
+    let baseURL = `https://api.unsplash.com/photos/?client_id=${GalleryKey}`;
+    //* Here Creating variable for API URL
+    const [picData, setPicdata] = useState([])
+    //* Here Creating state variable for data holding
     useEffect(() => {
-        axios.get(`${baseURL}`).then((response) => {
-            setData(response.data);
-            { console.log("data:", data) }
-        });
-    }, []);
+        axios.get(`${baseURL}`).then(
+            response =>
+                //* you don't need to add {} and return keyword if you are using this syntax 
+                setPicdata(response.data)
+        )
+            .catch(error =>
+                console.log(error)
+            )
+    }, [])
+    //* Here in useEffect it will run the code as its load and in the last we need to give it a dependency array if or useEffect is dependent on any change
     return (
         <section className="text-gray-600 body-font">
-            <div className="container px-5 py-24 mx-auto">
-                <div className="flex flex-wrap -m-4">
-                    {/* {data[0].map((e, index) => { */}
-                    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a className="block relative h-48 rounded overflow-hidden"  >
-                            {/* {console.log('e.urls.full: ', e.urls.full)} */}
-                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://placehold.co/600x400" />
-                            {/* <img alt="ecommerce" className="object-cover object-center w-full h-full block" src={data[0].urls.full} /> */}
-                            {/* {console.log('data[0]: ', data[0].urls)} */}
-                        </a>
+            {console.log('picData: ', picData)}
+            <h1 className=" text-4xl text-center py-4">Gallery App By using Unsplash API</h1>
+            <div className="container px-5 py-4 mx-auto flex flex-wrap">
+                {picData.map((obj, index) =>
+                    <div key={index} className="flex flex-wrap w-1/2">
+                        <div className="md:p-2 p-1 w-full">
+                            <img alt="gallery" className="w-full h-full object-cover object-center block" src={obj.urls.regular} />
+                            {/* <img alt="gallery" className="w-full h-full object-cover object-center block" src="https://dummyimage.com/601x361" /> */}
+                        </div>
                     </div>
-                    {/* })} */}
-                </div></div></section>
 
-    );
+
+                )}
+
+            </div>
+        </section>
+    )
 };
 
 export default Gallery;
